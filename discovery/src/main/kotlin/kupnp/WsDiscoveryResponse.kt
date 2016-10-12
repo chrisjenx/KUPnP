eecda9ffe7356e823e45a280e3871095c7b189a5
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2016-10-12 15:09:50 +0100

    added more test responses

--- discovery/src/main/kotlin/kupnp/WsDiscoveryResponse.kt
+++ discovery/src/main/kotlin/kupnp/WsDiscoveryResponse.kt
@@ -7,10 +7,11 @@ class WsDiscoveryResponse {
 
 
     companion object {
-        /**
-         * Matches the message uuid from the device, this is unique to the device.
-         */
         internal val REGEX_MESSAGE_ID = "<[a-z]+?:MessageID>(urn:)?uuid:([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})</[a-z]+?:MessageID>".toRegex(RegexOption.IGNORE_CASE)
+        /**
+         * Matches the address uuid from the device, this is unique to the device. (Should never change)
+         */
+        internal val REGEX_ADDRESS_ID = "<[a-z]+?:Address>(urn:)?uuid:([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})</[a-z]+?:Address>".toRegex(RegexOption.IGNORE_CASE)
     }
 
 
